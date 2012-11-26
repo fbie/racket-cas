3242d88b7ed5080c526d575ac6bf6188446ba2dc
Florian Biermann <fbie@itu.dk>
2012-11-26 13:22:54 +0100

    make bgn always the first match case

--- queue.rkt
+++ queue.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-;; the empty begin of the queue
+;; the empty beginning of the queue
 (struct bgn ())
 
 ;; enq is trivial, as it is simply a
@@ -10,21 +10,21 @@
 (define
   (get-first queue)
   (match queue
+    [(bgn) (#f)]
     [(enq val tail)
      (match tail
        [(bgn) val]
        [(enq _ _) (get-first tail)])]
-    [(bgn) (#f)]
     )
   )
 
 (define
   (deq queue)
   (match queue
+    [(bgn) (#f)]
     [(enq val tail)
      (match tail
        [(bgn) tail] ;; remove the first item in queue
        [(enq _ _) (enq val (deq tail))])]
-    [(bgn) (#f)]
     )
   )
