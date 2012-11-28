2c132487e8e3882c99f309a5446abf0614264e50
Florian Biermann <fbie@itu.dk>
2012-11-28 18:29:19 +0100

    add first implementation of CAS on atomic type

---
+++ atomic-ref.rkt
@@ -0,0 +1,19 @@
+#lang racket
+
+(struct atomic (sem ref))
+
+(define
+  (make-atomic ref)
+  (atomic (make-semaphore 1) ref)
+  )
+
+(define
+  (CAS ref exp new)
+  (define 
+    (cas)
+    (if (equal? (atomic-ref ref) exp)
+      [atomic (atomic-sem ref) new]
+      [#f]
+      ))
+  (call-with-semaphore (atomic-sem ref) cas)
+  )
