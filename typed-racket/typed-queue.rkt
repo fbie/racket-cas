c06bd3ea1e733231636695b0b3ab0306956f88c0
Florian Biermann <fbie@itu.dk>
2012-11-30 18:31:49 +0100

    add working enqueue for michael-scott-queue

--- typed-racket/typed-queue.rkt
+++ typed-racket/typed-queue.rkt
@@ -1,32 +1,54 @@
 #lang typed/racket
 
-(define-type (Queue A) (U bgn (enq A)))
+;; the node type of which the queue actually consists
+(define-type QNode (qnode))
+(struct: (A) qnode ([value : A] [[next : QNode] : #:mutable]))
 
-;; the empty beginning of the queue
-(struct: bgn ())
-
-;; enq is trivial, as it is simply a
-;; constructor extending an existing queue
-(struct: (A) enq ([val : A] [tail : (Queue A)]))
-
-(: get-first (All (A) (Queue A) -> (U A #f)))
-(define (get-first queue)
-  (match queue
-    [(bgn) #f]
-    [(enq val tail)
-     (match tail
-       [(bgn) val]
-       [(enq _ _) (get-first tail)])]
+(define (find-end qn)
+  (let ([next (qnode-next qn)])
+    (if next
+        [find-end next]
+        qn)
     )
   )
 
-(: deq (All (A) (Queue A) -> (Queue A)))
-(define (deq queue)
-  (match queue
-    [(bgn) queue] ;; nothing to remove
-    [(enq val tail)
-     (match tail
-       [(bgn) tail] ;; remove the first item in queue
-       [(enq _ _) (enq val (deq tail))])]
+(define (enq val qn)
+  (let ([end (find-end qn)])
+    (set-qnode-next! end (qnode val #f))
     )
   )
+
+(define (len qn)
+  (if (not (equal? qn void))
+      (add1 (len (qnode-next qn)))
+      0)
+  )
+
+;; a container type for the linked list
+(struct queue (head [tail #:mutable]))
+
+(define (make-queue)
+  (let ([head (qnode void void)])
+    (queue head head)
+    )
+  )
+
+(define (enqueue value q)
+  (let ([new (qnode value void)])
+    (set-qnode-next! (queue-tail q) new)
+    (set-queue-tail! q new)
+    )
+  )
+
+(define (dequeue q)
+  (when (not (equal? (queue-head q) (queue-tail q)))
+    (let ([head (queue-head q)])
+      (set-qnode-next! head (qnode-next (qnode-next head)))
+      )
+    )
+  )
+
+(define (size q)
+  (len (qnode-next (queue-head q)))
+  )
+
