4f258730dba222b7a69d8194c283ca3541e04679
Florian Biermann <fbie@itu.dk>
2012-11-30 14:47:13 +0100

    CAS now executing in-place

--- racket/atomic-ref.rkt
+++ racket/atomic-ref.rkt
@@ -1,16 +1,15 @@
 #lang racket
 
-(struct atomic (sem ref))
+(struct atomic (sem [ref #:mutable]))
 
 (define (make-atomic ref)
   (atomic (make-semaphore 1) ref)
   )
 
 (define (CAS ref exp new)
-  (define 
-    (cas)
+  (define (cas)
     (if (equal? (atomic-ref ref) exp)
-      (atomic (atomic-sem ref) new)
+      [and (set-atomic-ref! ref new) #t]
       #f
       )
     )
