aa30d3608a9073b573d388fb78cd90ca4f7cec2b
Florian Biermann <fbie@itu.dk>
2012-11-30 16:12:22 +0100

    replace queue by proper in-place queue

--- racket/queue.rkt
+++ racket/queue.rkt
@@ -1,28 +1,52 @@
 #lang racket
 
-;; the empty beginning of the queue
-(struct bgn ())
+;; the node type of which the queue actually consists
+(struct qnode (value [next #:mutable]))
 
-;; enq is trivial, as it is simply a
-;; constructor extending an existing queue
-(struct enq (val tail))
-
-(define (get-first queue)
-  (match queue
-    [(bgn) (#f)]
-    [(enq val tail)
-     (match tail
-       [(bgn) val]
-       [(enq _ _) (get-first tail)])]
+(define (find-end qn)
+  (let ([next (qnode-next qn)])
+    (if next
+        [find-end next]
+        qn)
     )
   )
 
-(define (deq queue)
-  (match queue
-    [(bgn) queue]
-    [(enq val tail)
-     (match tail
-       [(bgn) tail] ;; remove the first item in queue
-       [(enq _ _) (enq val (deq tail))])]
+(define (enq val qn)
+  (let ([end (find-end qn)])
+    (set-qnode-next! end (qnode val #f))
     )
+  )
+
+(define (len qn)
+  (if (not (equal? qn void))
+      (add1 (len (qnode-next qn)))
+      0)
+  )
+
+;; a container type for the linked list
+(struct queue (head [tail #:mutable]))
+
+(define (make-queue)
+  (let ([head (qnode void void)])
+    (queue head head)
+    )
+  )
+
+(define (enqueue value q)
+  (let ([new (qnode value void)])
+    (set-qnode-next! (queue-tail q) new)
+    (set-queue-tail! q new)
+    )
+  )
+
+(define (dequeue q)
+  (when (not (equal? (queue-head q) (queue-tail q)))
+    (let ([head (queue-head q)])
+      (set-qnode-next! head (qnode-next (qnode-next head)))
+      )
+    )
+  )
+
+(define (size q)
+  (len (qnode-next (queue-head q)))
   )
