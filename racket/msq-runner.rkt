fffb4bba340b52e961fd4ee65905ec8ab6e8b299
Florian Biermann <fbie@itu.dk>
2012-12-03 14:29:16 +0100

    add first iteration of test suite

---
+++ racket/msq-runner.rkt
@@ -0,0 +1,62 @@
+#lang racket
+
+(require rackunit)
+(require (planet dyoo/while-loop))
+
+(require "michael-scott-queue.rkt")
+
+(define (enqueue-each n q)
+  (enqueue n q)
+  (when (> n 1) (enqueue-each (sub1 n) q))
+  )
+
+(define (dequeue-each n q)
+  (dequeue q)
+  (when (> n 1) (dequeue-each (sub1 n) q))
+  )
+
+(define (enq-deq-each n q)
+  (let [(n1 n)]
+    (enqueue-each n1 q))
+  (let [(n2 n)]
+    (dequeue-each n2 q))
+  )
+
+(define (func-of-size f s q)
+  (define (func)
+    (f s q))
+  func
+  )
+
+(define (run-n-threads n f)
+  (if (> n 0)
+      (cons (thread f) (run-n-threads (sub1 n) f))
+      null)
+  )
+
+(define (run-tests n f s q)
+  (let ([chunk (/ s n)])
+    (run-n-threads n (func-of-size f chunk q))
+    )
+  )
+
+(define (block-run threads)
+  (when (not (null? threads))
+    (block-run (cdr threads))
+    (while (not (thread-dead? (car threads)))
+           void))
+  )
+
+(define (all-tests t s)
+  (define q (make-msq))
+  (block-run (run-tests t enqueue-each s q))
+  (check-eq? (size q) test-size)
+  (block-run (run-tests t dequeue-each s q))
+  (check-eq? (size q) 0)
+  (block-run (run-tests t enq-deq-each s q))
+  (check-eq? (size q) 0)
+  )
+
+(define test-size 1000000)
+
+(all-tests 4 test-size)
