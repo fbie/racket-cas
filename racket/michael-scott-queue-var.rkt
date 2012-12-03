d8d249a891326ab66703c7e07a827d7e999798b6
Florian Biermann <fbie@itu.dk>
2012-12-03 10:34:25 +0100

    add msq variant from Turon et al. 2013

---
+++ racket/michael-scott-queue-var.rkt
@@ -0,0 +1,48 @@
+#lang racket
+
+(require (planet dyoo/while-loop))
+(require "atomic-ref.rkt")
+
+;; the node type of which the queue actually consists
+(struct node (value [next #:mutable]))
+
+(define (len qn)
+  (if (not (equal? qn void))
+      (add1 (len (atomic-ref (node-next qn))))
+      0)
+  )
+  
+;; a container type for the linked list
+(struct msq ([head #:mutable]))
+
+(define (make-msq)
+  (let ([head (node void (make-atomic void))])
+    (msq (make-atomic head))
+    )
+  )
+
+(define (enqueue value q)
+  (let ([n (node value (make-atomic void))])
+    (define (try c)
+      (if (equal? (atomic-ref (node-next c)) void)
+          (when (not (CAS (node-next c) void n))
+              (try c))
+          (try (atomic-ref (node-next c))))
+      )
+      (try (atomic-ref (msq-head q))))
+  )
+
+(define (dequeue q)
+  (define (try)
+    (let ([n (atomic-ref (msq-head q))])
+      (if (equal? (atomic-ref (node-next n)) void)
+          void
+          (if (CAS (msq-head q) n (atomic-ref (node-next n)))
+              (node-value (atomic-ref (node-next n)))
+              (try)))))
+  (try)
+  )
+  
+(define (size q)
+  (len (atomic-ref (node-next (atomic-ref (msq-head q)))))
+  )
