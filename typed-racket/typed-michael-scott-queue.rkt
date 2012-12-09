ba053fc674d33fd145aff3ccbcf37cea44f7bb03
Florian Biermann <fbie@itu.dk>
2012-12-09 10:15:20 +0100

    type msq

--- typed-racket/typed-michael-scott-queue.rkt
+++ typed-racket/typed-michael-scott-queue.rkt
@@ -1,5 +1,4 @@
 #lang typed/racket
-
 (require "typed-atomic-ref.rkt")
 
 ;; the node type of which the queue actually consists
@@ -15,7 +14,7 @@
 
 ;; a container type for the linked list
 (define-type MS-Queue msq)
-(struct: msq ([head : (AtomicRef node)] [tail : (AtomicRef Node)]))
+(struct: msq ([head : (AtomicRef (U Void Node))] [tail : (AtomicRef (U Void Node))]))
 
 (: make-msq (-> MS-Queue))
 (define (make-msq)
@@ -29,7 +28,7 @@
   (: try ( -> Void))
   (define (try)
     (let* [(node (node value (make-atomic (void))))
-           (tail (atomic-ref (msq-tail q)))
+           (tail (cast (atomic-ref (msq-tail q)) Node))
            (next (atomic-ref (node-next tail)))]
       (if (equal? tail (msq-tail q))
         (if (equal? next (void))
@@ -48,8 +47,8 @@
   (: try (-> (U Void Any)))
   (define (try)
          (let* [(head (msq-head q))
-                (tail (msq-tail q))
-                (next (atomic-ref (node-next (atomic-ref head))))]
+                 (tail (msq-tail q))
+                 (next (atomic-ref (node-next (cast (atomic-ref head) Node))))]
            (if (equal? head (msq-head q))
              (if (equal? (atomic-ref head) (atomic-ref tail))
                  (if (equal? next (void))
@@ -65,7 +64,7 @@
 
 (: size (MS-Queue -> Integer))
 (define (size q)
-  (len (atomic-ref (node-next (atomic-ref (msq-head q)))))
+  (len (atomic-ref (node-next (cast (atomic-ref (msq-head q)) Node))))
   )
 
 (provide
