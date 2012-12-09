8b65aef9cd844319f9e1ace4734443e36195ace6
Florian Biermann <fbie@itu.dk>
2012-12-09 01:31:37 +0100

    close to finishing typed msq

--- typed-racket/typed-michael-scott-queue.rkt
+++ typed-racket/typed-michael-scott-queue.rkt
@@ -1,26 +1,30 @@
-#lang racket
+#lang typed/racket
 
 (require "typed-atomic-ref.rkt")
 
 ;; the node type of which the queue actually consists
-(struct node (value next))
+(struct: node ([value : (U Void Any)] [next : (AtomicRef (U Void node))]))
 
+(: len (All (A) (U node Void) -> Integer))
 (define (len qn)
   (if (not (equal? qn (void)))
-      (add1 (len (atomic-ref (node-next qn))))
+      (add1 (len (atomic-ref (node-next (cast qn node)))))
       0)
   )
 
 ;; a container type for the linked list
-(struct msq (head tail))
+(struct: msq ([head : (AtomicRef node)] [tail : (AtomicRef node)]))
 
+(: make-msq (-> msq))
 (define (make-msq)
   (let ([head (node (void) (make-atomic (void)))])
     (msq (make-atomic head) (make-atomic head))
     )
   )
 
+(: enqueue (Any msq -> Void))
 (define (enqueue value q)
+  (: try ( -> Void))
   (define (try)
     (let* [(node (node value (make-atomic (void))))
            (tail (msq-tail q))
@@ -30,14 +34,16 @@
           (if (CAS (node-next (atomic-ref tail)) next node)
               (let ([a (CAS (msq-tail q) (atomic-ref tail) node)])
                 (void))
-              (let ([a (CAS (msq-tail q) tail next)])
+              (let ([a (CAS (msq-tail q) (atomic-ref tail) next)])
                 (try)))
           (try))
         (try))))
   (try)
   )
 
+(: dequeue (msq -> Any))
 (define (dequeue q)
+  (: try (-> (U Void Any)))
   (define (try)
          (let* [(head (msq-head q))
                 (tail (msq-tail q))
@@ -46,22 +52,22 @@
              (if (equal? (atomic-ref head) (atomic-ref tail))
                  (if (equal? next (void))
                      (void)
-                     (let ([a (CAS (msq-tail q) tail next)])
+                     (let ([a (CAS (msq-tail q) (atomic-ref tail) next)])
                        (try)))
                  (if (CAS (msq-head q) (atomic-ref head) next)
-                   (node-value next)
+                   (node-value (cast next node))
                    (try)))
              (try))))
   (try)
   )
-  
+
+(: size (msq -> Integer))
 (define (size q)
   (len (atomic-ref (node-next (atomic-ref (msq-head q)))))
   )
 
 (provide
- (contract-out
-  [make-msq (-> msq?)]
-  [enqueue (-> any/c msq? void?)]
-  [dequeue (-> msq? (or/c any/c void?))]
-  [size (-> msq? integer?)]))
+ make-msq
+ enqueue
+ dequeue
+ size)
